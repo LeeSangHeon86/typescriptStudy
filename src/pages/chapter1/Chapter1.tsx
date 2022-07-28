53452bc7f2b52ab0fe128ac92fcd8f7f124a1101
이상헌 <bleach8625@gmail.com>
2022-07-29 01:11:18 +0900

    Prettier 설정

--- src/pages/chapter1/Chapter1.tsx
+++ src/pages/chapter1/Chapter1.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 function Chapter1() {
+  const value = '이상헌';
+
+  function test1() {
+    function test2() {
+      function test3() {
+        function test4() {
+          function test5() {}
+        }
+      }
+    }
+  }
+
+  const obj = { test1: '1', test2: 2 };
+
   return (
     <div>
       <div>Chapter1</div>
